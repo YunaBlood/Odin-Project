30d2d5470a69ddaa362c09a5925ac0f22a1ba861
Valentin <52529145+YunaBlood@users.noreply.github.com>
2024-11-13 02:18:41 +0100

    Refactor downcase method for word and result

--- mini-project/subtrings.rb/substrings.rb
+++ mini-project/subtrings.rb/substrings.rb
@@ -4,15 +4,13 @@ def substrings(string, dictionary)
 
   lower_string = string.downcase.gsub(/[^A-Za-z0-9]/, "")
 
-  dictionary_lowecase = dictionary.map {|word| word.downcase}
+  result = Hash.new(0)
 
-  dictionary_hash = Hash.new(0)
-
-  dictionary_lowecase.each do |word|
-    count_word = lower_string.scan(word).length
-    count_word > 0 ? dictionary_hash[word] += count_word : nil
+  dictionary.each do |word|
+    count_word = lower_string.scan(word.downcase).size
+    result[word.downcase] += count_word if count_word > 0
   end
-  dictionary_hash
+  result
 end
 
 puts substrings("Howdy partner, sit down! How's it going?", dictionary)
